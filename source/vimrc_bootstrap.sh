46e6a3bcfa4dabbb2bd8f371df3bd1487598d20c
Josh Leder <josh@ha.cr>
2015-01-13 11:41:54 -0600

    test for the directory before bootstrapping vundle

--- source/vimrc_bootstrap.sh
+++ source/vimrc_bootstrap.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-git clone -b 0.9.1 https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
+if [[ ! -d ~/.vim/bundle/vundle ]]; then
+    git clone -b 0.9.1 https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
+fi
