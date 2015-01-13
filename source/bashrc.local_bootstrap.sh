10da027f9ea4cccb85ef1f87771d3bc5ddb37deb
Josh Leder <josh@ha.cr>
2015-01-13 14:41:35 -0600

    local bashrc

---
+++ source/bashrc.local_bootstrap.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if [ -e ~/.bashrc ] && ! grep -l '.bashrc.local' ~/.bashrc >/dev/null ; then
+    echo "Sourcing \`.bashrc.local\` from within ~/.bashrc"
+
+    echo -e "\n# Source local bashrc"        >> ~/.bashrc
+    echo 'if [[ -r ~/.bashrc.local ]]; then' >> ~/.bashrc
+    echo '  source ~/.bashrc.local'          >> ~/.bashrc
+    echo 'fi'                                >> ~/.bashrc
+fi
